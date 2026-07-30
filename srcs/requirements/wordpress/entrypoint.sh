a3d758cbd8a960c9e96a814780d89a5943c63407
ML <louism0495@gmail.com>
2026-07-30 13:24:19 +0200

    fix wordpress autoinstall

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+set -ex
+
 
 # TODO: get fmp version
-php-fpm8.2
+PHP_VERSION="$(ls /etc/php/ | tail -1)"
+PHP_CMD="php-fpm${PHP_VERSION} -F"
+
+# wp core download --allow-root
+
+wp config create \
+	--dbname="$WORDPRESS_DB_NAME" \
+	--dbuser="$WORDPRESS_DB_USER" \
+	--dbpass="$WORDPRESS_DB_PASSWORD" \
+	--dbhost="$WORDPRESS_DB_HOST" \
+	--allow-root
+
+if ! wp core is-installed; then
+	wp core install \
+		--url="https://mlouis.42.fr" \
+		--title="WIP" \
+		--admin_user="$USER_ADMIN" \
+		--admin_password="$PASSWORD_ADMIN" \
+		--admin_email="$EMAIL_ADMIN" \
+		--allow-root
+fi
+
+${PHP_CMD}
+
