439f35f6563735c208068dd3fd5f5eabc593a34f
ML <louism0495@gmail.com>
2026-08-04 12:26:44 +0200

    fix wp with allow-root and fix mariadb restart

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -15,22 +15,19 @@ if [ ! -e "/var/www/html/wp-config.php" ]; then
 		--dbpass="$WORDPRESS_DB_PASSWORD" \
 		--dbhost="$WORDPRESS_DB_HOST"
 
-	# if ! sudo -u www-data wp core is-installed; then
-	if ! wp core is-installed; then
-		wp core install \
+	if ! sudo -u www-data wp core is-installed; then
+		sudo -u www-data wp core install \
 			--url="https://mlouis.42.fr" \
 			--title="WIP" \
 			--admin_user="$USER_ADMIN" \
 			--admin_password="$PASSWORD_ADMIN" \
-			--admin_email="$EMAIL_ADMIN" \
-			--allow-root
+			--admin_email="$EMAIL_ADMIN"
 	fi
 
-	wp user create \
+	sudo -u www-data wp user create \
 		"$USER_BILLY" \
 		"$EMAIL_BILLY" \
-		--user_pass="$PASSWORD_BILLY" \
-		--allow-root
+		--user_pass="$PASSWORD_BILLY"
 
 fi
 
